847d452f2fb06b70349d6ec421879776b58a16ec
ltenore <leightenore@gmail.com>
2018-12-24 14:47:28 -0700

    added second js file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,47 +12,47 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-//Click event for submitting user location
+//Event listener for submitting user location
 document.addEventListener("keyup", function (event) {
     if (event.keyCode === 13) {
       event.preventDefault();
 
       var location = $("#search_bar").val().trim();
       var queryURL = "http://www.mapquestapi.com/geocoding/v1/address?key=sVLMqoRolFyhsmbAGzECprYrQinTd4CB&location=" + location;
-      
-      //AJAX Get Request
+
+      //AJAX Get Request -- Mapquest API
       $.ajax({
         url: queryURL,
         method: "GET"
       })
       .then(function(response) {
-        console.log(queryURL);
+        console.log("Query URL: " + queryURL);
         console.log(response);
-
-        console.log(response.results[0].locations[0].latLng.lat);
-        console.log(response.results[0].locations[0].latLng.lng);
-
+        
         var latitude = response.results[0].locations[0].latLng.lat;
         var longitude = response.results[0].locations[0].latLng.lng;
-        // // var riseTime;
-        // // var duration;
+        // var riseTime;
+        // var duration;
+
+        console.log("Latitude: " + latitude);
+        console.log("Longitude: " + longitude);
   
         var locationInfo = {
             location: location,
             latitude: latitude,
             longitude: longitude,
-        //     // riseTime: riseTime,
-        //     // duration: duration
+            // riseTime: riseTime,
+            // duration: duration
           };
         
         //Pushes location object to Firebase
         database.ref().push(locationInfo);
-        
-        $("#search_bar").val("");
-        $("#search_bar").removeAttr('placeholder');
+
+        //Changes window to results page
+        window.location.replace("results.html");
       });
-    }
-});
+    };
+  });
 
 //rocket man interaction
 var intervaId;
