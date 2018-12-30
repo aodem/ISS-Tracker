83eeca9e2f416b535c1b089740d846944af7b440
ltenore <leightenore@gmail.com>
2018-12-29 17:24:50 -0700

    fixed merge errors

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,110 +1,12 @@
-
-$(document).ready(function(){
-
-  //Initialize Firebase
-  var config = {
-      apiKey: "AIzaSyCTB2ONqGvJBBqRGg7FMr2Q8xfnH0Ejdqo",
-      authDomain: "project-1-904b8.firebaseapp.com",
-      databaseURL: "https://project-1-904b8.firebaseio.com",
-      projectId: "project-1-904b8",
-      storageBucket: "",
-      messagingSenderId: "356456726650"
-    };
-
-  firebase.initializeApp(config);
-
-  var database = firebase.database();
-
-  //Click event for submitting user location
-  $("#searchButton").on("click", function(event) {
-      event.preventDefault();
-
-      var location = $("#location-input").val().trim();
-      var latitude;
-      var longitude;
-      var riseTime;
-      var duration;
-
-      var locationInfo = {
-          location: location,
-          latitude: latitude,
-          longitude: longitude,
-          riseTime: riseTime,
-          duration: duration
-        };
-      
-      var queryURL = "http://www.mapquestapi.com/geocoding/v1/address?key=sVLMqoRolFyhsmbAGzECprYrQinTd4CB&location=" + location;
-      
-      //Pushes location object to Firebase
-      database.ref().push(locationInfo);
-
-      //AJAX Get Request
-      $.ajax({
-          url: queryURL,
-          method: "GET"
-        })
-          .then(function(response) {
-            console.log(queryURL);
-            console.log(response);
-          });
-
-     //history list
-     let histArray = [];
-     if(histArray.length < 6){
-       histArray.unshift(location); 
-     }else{
-       histArray.pop();
-       histArray.unshift(location);
-     }     
-
-     let listDiv = $("<div>");
-     let listShell = $("<ul>");
-
-     for(let i = 0; i < histArray.length; i++){
-       listItem = listShell.append("<li>" + histArray[i] + "</li>")
-     }
-
-     listDiv.empty();
-     listDiv.append(listShell);
-
-     $(".tabContent").html(listDiv);
-  });
-
-  //rocket man
-  var intervaId;
-
-  intervalId = setInterval(() => { $('#rocketMan').tooltip('show')}, 3000)
-
-  $('#rocketMan').on("click", function(){
-      console.log("hi!")
-      $('#rocketMan').tooltip('hide')
-      clearInterval(intervalId);
-      $('#rocketMan').attr('title', 'Random Info!')
-      $('#rocketMan').popover('show')
-  });
-
-
-  //tab animations
-  $(".nav_tab_body").on("click", function(){
-    console.log("hi!");
-    if($(this).attr("data-state") === 'still'){
-      $(this).addClass("tabsSlide");
-      $(this).attr('data-state', 'animate');
-    }else if($(this).attr("data-state") === 'animate'){
-      $(this).removeClass("tabsSlide")
-      $(this).attr("data-state", 'still');
-    }
-  });
-
 //Initialize Firebase
 var config = {
-    apiKey: "AIzaSyCTB2ONqGvJBBqRGg7FMr2Q8xfnH0Ejdqo",
-    authDomain: "project-1-904b8.firebaseapp.com",
-    databaseURL: "https://project-1-904b8.firebaseio.com",
-    projectId: "project-1-904b8",
-    storageBucket: "",
-    messagingSenderId: "356456726650"
-  };
+  apiKey: "AIzaSyCTB2ONqGvJBBqRGg7FMr2Q8xfnH0Ejdqo",
+  authDomain: "project-1-904b8.firebaseapp.com",
+  databaseURL: "https://project-1-904b8.firebaseio.com",
+  projectId: "project-1-904b8",
+  storageBucket: "",
+  messagingSenderId: "356456726650"
+};
 
 firebase.initializeApp(config);
 
@@ -112,55 +14,42 @@ var database = firebase.database();
 
 //Event listener for submitting user location
 document.addEventListener("keyup", function (event) {
-    if (event.keyCode === 13) {
-      event.preventDefault();
+  if (event.keyCode === 13) {
+    event.preventDefault();
 
-      var location = $("#search_bar").val().trim();
-      var queryURL = "http://www.mapquestapi.com/geocoding/v1/address?key=sVLMqoRolFyhsmbAGzECprYrQinTd4CB&location=" + location;
+    var location = $("#search_bar").val().trim();
+    var queryURL = "http://www.mapquestapi.com/geocoding/v1/address?key=sVLMqoRolFyhsmbAGzECprYrQinTd4CB&location=" + location;
 
-      //AJAX Get Request -- Mapquest API
-      $.ajax({
-        url: queryURL,
-        method: "GET"
-      })
-      .then(function(response) {
-        console.log("Query URL: " + queryURL);
-        console.log(response);
-        
-        var latitude = response.results[0].locations[0].latLng.lat;
-        var longitude = response.results[0].locations[0].latLng.lng;
-        // var riseTime;
-        // var duration;
+    //AJAX Get Request -- Mapquest API
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+    .then(function(response) {
+      console.log("Query URL: " + queryURL);
+      console.log(response);
+      
+      var latitude = response.results[0].locations[0].latLng.lat;
+      var longitude = response.results[0].locations[0].latLng.lng;
+      // var riseTime;
+      // var duration;
 
-        console.log("Latitude: " + latitude);
-        console.log("Longitude: " + longitude);
-  
-        var locationInfo = {
-            location: location,
-            latitude: latitude,
-            longitude: longitude,
-            // riseTime: riseTime,
-            // duration: duration
-          };
-        
-        //Pushes location object to Firebase
-        database.ref().push(locationInfo);
+      console.log("Latitude: " + latitude);
+      console.log("Longitude: " + longitude);
 
-        //Changes window to results page
-        window.location.replace("results.html");
-      });
-    };
-  });
+      var locationInfo = {
+          location: location,
+          latitude: latitude,
+          longitude: longitude,
+          // riseTime: riseTime,
+          // duration: duration
+        };
+      
+      //Pushes location object to Firebase
+      database.ref().push(locationInfo);
 
-//rocket man interaction
-var intervaId;
-
-intervalId = setInterval(() => { $('#rocketMan').tooltip('show')}, 3000)
-
-$(document).on("click", function(){
-    console.log(this)
-    $('#rocketMan').tooltip('hide')
-    clearInterval(intervalId);
-    $('#rocketMan').attr('title', 'Random Info!')
-    $('#rocketMan').popover('show')
+      //Changes window to results page
+      window.location.replace("results.html");
+    });
+  };
 });
